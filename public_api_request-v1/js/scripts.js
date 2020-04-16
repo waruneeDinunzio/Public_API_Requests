4b8cd522f4cd55c9a37303ccf8c1007c97f05d1f
waruneeDinunzio <56564699+waruneeDinunzio@users.noreply.github.com>
2020-04-15 22:48:41 -0400

    problem with modal close button
    re-write code to make it short and clean

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -9,18 +9,10 @@ const form = document.createElement('form');
 form.action = "#";
 form.method = "get";
 searchContainer.appendChild(form);
-const searchInput = document.createElement('input');
-searchInput.type = "search";
-searchInput.id = "search-input";
-searchInput.className = "search-input";
-searchInput.placeholder = "Search...";
-form.appendChild(searchInput);
-const submit = document.createElement('input');
-submit.type = "submit";
-submit.value = "&#x1F50D;";
-submit.id = "search-submit";
-submit.className = "search-submit";
-form.appendChild(submit);
+form.innerHTML= `
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    `
 // 
 //  FETCH FUNCTIONS
 // ------------------------------------------
@@ -40,22 +32,27 @@ function displayUsers(data) {
   card.className = "card";
   gallery.appendChild(card);
   card.innerHTML = `
-  <div class="card-img-container">
-          <img class="card-img" src="${data.picture.large}" alt="profile picture">
-          </div>
-            <div class="card-info-container">
-              <h3 id="name" class="card-name cap">${data.name.first}`+ " " + `${data.name.last}</h3>
-              <p class="card-text">${data.email}</p>
-              <p class="card-text cap">${data.location.city}</p>
-            </div>
+    <div class="card-img-container">
+        <img class="card-img" src="${data.picture.large}" alt="profile picture">
+    </div>
+        <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${data.name.first}`+ " " + `${data.name.last}</h3>
+          <p class="card-text">${data.email}</p>
+          <p class="card-text cap">${data.location.city}</p>
+    </div>
   `;
 });
 }
-/*
+
 function displayModal(data, i) {
-  data.map( data =>{ 
+  //data.map(data =>{
+let current_datetime = new Date(`${data[i].dob.date}`);
+console.log(current_datetime);
+let formatted_date = current_datetime.getDate() + "/" + (current_datetime.getMonth() + 1) + "/" + current_datetime.getFullYear()
+console.log(formatted_date)
+//var DateCreated = new Date(Date.parse(${data[i].dob.date})).format("MM/dd/yyyy"); 
   const modalContainer = document.createElement('div');
-  modalContainer.className = "modalContainer";
+  modalContainer.className = "modal-container";
   body.appendChild(modalContainer);
   modalContainer.innerHTML = 
   `
@@ -67,9 +64,13 @@ function displayModal(data, i) {
           <p class="modal-text">${data[i].email}</p>
           <p class="modal-text cap">${data[i].location.city}</p>
           <hr>
-          <p class="modal-text">(555) 555-5555</p>
-          <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-          <p class="modal-text">Birthday: 10/21/2015</p>
+          <p class="modal-text">${data[i].phone}</p>
+          <p class="modal-text">${data[i].location.street.number}`+ " "+`
+                                ${data[i].location.street.name}` + ". " +`
+                                ${data[i].location.city}` + ", " +`
+                                ${data[i].location.state}` + " " +`
+                                ${data[i].location.postcode}</p>
+          <p class="modal-text">${formatted_date}</p>
       </div>
   </div>
   <div class="modal-btn-container">
@@ -77,50 +78,20 @@ function displayModal(data, i) {
       <button type="button" id="modal-next" class="modal-next btn">Next</button>
   </div>
   `;
-  });
-}*/
 
-  function displayModal(data, i) {
-    //for (let i=0; i < data.length; i++ ) {
-      const modalContainer = document.createElement('div');
-      modalContainer.className = "modal-container";
-      body.appendChild(modalContainer)[i];
-      const modal = document.createElement('div');
-      modal.className = "modal";
-      modalContainer.appendChild(modal)[i];
-      //modal.style.display = 'none';
-      const button = document.createElement('button');
-      button.type = "button";
-      button.id = "modal-close-btn";
-      button.className = "modal-close-btn";
-      button.innerHTML = "<strong>X</strong>";
-      modal.appendChild(button)[i];
-      const modalInfo = document.createElement('div');
-      modalInfo.className = "modal-info-container";
-      modal.appendChild(modalInfo)[i];
-      const modalImg = document.createElement("img");
-      modalImg.className = "modal-img";
-      modalImg.src = `${data[i].picture.large}`;
-      modalImg.alt = "profile picture";
-      modalInfo.appendChild(modalImg)[i];
-      const modalH3 = document.createElement("h3");
-      modalH3.id = "name";
-      modalH3.className = "modal-name cap";
-      modalH3.innerHTML = `${data[i].name.first}`+ " " + `${data[i].name.last}`;
-      modalInfo.appendChild(modalH3)[i];
-      const modalEmail = document.createElement("p");
-      modalEmail.className = "card-text";
-      modalEmail.innerHTML = `${data[i].email}`;
-      modalInfo.appendChild(modalEmail)[i];
-      const modalCity = document.createElement("p");
-      modalCity.className = "card-text cap";
-      modalCity.innerHTML = `${data[i].location.city}`;
-      modalInfo.appendChild(modalCity)[i];
-    //}
-    //if (modalH3.innerHTML === 
-  }
+  const button = document.querySelector('.modal-close-btn');
+  //const modalContainer = document.querySelector('.modal-container');
+  button.onclick = function() {
+    modalContainer.style.display = "none";
+  };
 
- 
+  window.onclick = function(event) {
+    if (event.target == modalContainer) {
+      modalContainer.style.display = "none";
+    }
+  };
+} 
+
 function eventListener(data) {
   let cards = document.querySelectorAll('.card');
     for (let i = 0; i < cards.length; i++) {
